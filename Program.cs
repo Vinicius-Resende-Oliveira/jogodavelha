b5ce380f027d4a0b3f918321074c77094ac0f3b0
Vinícius Oliveira <vinicius_rr1@hotmail.com>
2021-06-16 00:34:02 -0300

    Arquivos Iniciais

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace jogodavelha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Velha velha = new Velha();
+            string partida = null;
+            int vencedor = 0;
+            
+            partida = "x:2,2 o:2,1 x:3,3 o:1,1 x:3,1 o:1,3 x:3,2";
+            vencedor = velha.QuemVenceu(partida);
+            Console.WriteLine("x venceu.: " + (vencedor == -1));
+            
+            partida = "x:1,1 o:3,3 x:2,2 o:3,1 x:3,2 o:1,2 x:2,1 x:2,3 x:1,3";
+            vencedor = velha.QuemVenceu(partida);
+            Console.WriteLine("deu velha: " + (vencedor == 0));
+            
+            partida = "x:1,1 o:2,3 x:3,3 o:2,2 x:3,1 o:2,1";
+            vencedor = velha.QuemVenceu(partida);
+            Console.WriteLine("o venceu.: " + (vencedor == 1));
+        }
+    }
+}
---
+++ Velha.cs
@@ -0,0 +1,45 @@
+namespace jogodavelha
+{
+  public class Velha
+  {
+
+    /// <summary>
+    ///
+    ///   O método analisa o registro de uma partida de jogo da velha e determina
+    ///   quem venceu.
+    ///
+    /// </summary>
+    ///
+    /// <param name="partida">
+    ///
+    ///   A partida é registrada numa sequência de coordenadas separadas por
+    ///   espaços:
+    ///   
+    ///     "x:2,2 o:2,1 x:3,3 o:1,1 x:3,1 o:1,3 x:3,2"
+    ///       
+    ///   Cada coordenada é formada da seguinte forma:
+    ///   
+    ///     - O símbolo do jogador seguido de dois pontos: 'x:' ou 'o:'
+    ///     - A coordenada da linha e da coluna escolhida pelo jogador.
+    ///       Os índices das linhas e das colunas variam entre 1 e 3.
+    ///
+    /// </param>
+    /// 
+    /// <returns>
+    ///
+    ///   O resultado é um inteiro identificando o empate ou o jogador vencedor da
+    ///   seguinte forma:
+    ///     -1  O jogador 'x' venceu a partida.
+    ///      0  Nenhum jogador venceu a partida.
+    ///      1  O jogador 'o' venceu a partida. 
+    ///
+    /// </returns>
+    public int QuemVenceu(string partida)
+    {
+      
+      // implemente o método...
+      
+      return 0;
+    }
+  }
+}
